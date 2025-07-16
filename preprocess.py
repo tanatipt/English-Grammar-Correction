fbe98b06ba5b63c717a15d3f28f4b408775820c3
tanatip_timtong <ttimt@sertiscorp.com>
2025-07-16 22:23:44 +0700

    feat: updated preprocessing to also create early stopping dataset

--- preprocess.py
+++ preprocess.py
@@ -5,9 +5,24 @@ from sklearn.model_selection import train_test_split
 pd.set_option('display.max_columns', 1000)
 pd.set_option('display.max_rows', 1000)
 
-def preprocess_df(df):
+def preprocess_df(df : pd.DataFrame) -> pd.DataFrame:
+    """
+    Preprocess the DataFrame by cleaning text columns and removing unwanted characters.
+
+    Args:
+        df (pd.DataFrame): DataFrame containing the columns 'modified' and 'sentence'.
+    """
     
-    def preprocess_text(column):
+    def preprocess_text(column : pd.Series) -> pd.Series:
+        """
+        Preprocess a text column by stripping whitespace, replacing multiple spaces with a single space,
+
+        Args:
+            column (pd.Series): Text column to preprocess.
+
+        Returns:
+            pd.Series: Preprocessed text column with unwanted characters removed.
+        """
         strip_col = column.str.strip()
         wsr_col = strip_col.str.replace(r"\s+", " ", regex=True)
         bad_punc_col = wsr_col.str.replace(r'"#%&\*\+/<=>@[\\]^{|}~_', '', regex=True)
@@ -18,17 +33,24 @@ def preprocess_df(df):
 
     return df
 
+# Load the data
 train_data = pd.read_parquet("source/train.parquet", engine='pyarrow')
 test_data  = pd.read_parquet("source/test.parquet",  engine='pyarrow')
 data = pd.concat([train_data, test_data]).drop(columns=['__index_level_0__', 'sec_transformation'])
+
+# Filter for English language and preprocess
 data = data[data['lang'] == 'en'].drop(columns="lang")
 preprocessed_data = preprocess_df(data).dropna()
 
-X_train, X_test = train_test_split(preprocessed_data,test_size=0.1, random_state=2543673, stratify=preprocessed_data['transformation'] )
-X_train, X_valid = train_test_split(X_train,  test_size=0.1, random_state=2543673, stratify=X_train['transformation'])
+# Split the data into train, validation, test, and early stopping datasets
+X_train, X_test = train_test_split(preprocessed_data,test_size=2000, random_state=2543673, stratify=preprocessed_data['transformation'] )
+X_train, X_valid = train_test_split(X_train,  test_size=2000, random_state=2543673, stratify=X_train['transformation'])
+X_train, X_es = train_test_split(X_train,train_size=2000, test_size=2000, random_state=2543673, stratify=X_train['transformation'])
 
+# Save the datasets to CSV files
 X_train.to_csv("preprocessed/train.csv", index=False)
 X_valid.to_csv("preprocessed/valid.csv", index=False)
 X_test.to_csv("preprocessed/test.csv", index=False)
+X_es.to_csv('preprocessed/es.csv', index = False)
             
             
